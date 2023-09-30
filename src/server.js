a4d490bbd25116040c3be77554a06ceaa2d590f7
Ronsou <samelsonjohn@protonmail.com>
2023-09-30 13:42:25 -0400

    testing proxy

--- src/server.js
+++ src/server.js
@@ -3,25 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Set up proxy middleware
-// Replace 'http://target-server.com' with the actual target server's URL
-const target = 'http://target-server.com';
+const target = 'http://jsonplaceholder.typicode.com'; // Ensure using 'http' not 'https'
 const proxyMiddleware = createProxyMiddleware({
   target,
-  changeOrigin: true, // needed for virtual hosted sites
-  ws: true, // proxy websockets
+  changeOrigin: true,
+  pathRewrite: { '^/api': '' }, // Remove the /api prefix before forwarding the request
 });
 
-// Apply proxy middleware to desired path
-// In this case, all requests to '/api' will be proxied to the target server
 app.use('/api', proxyMiddleware);
 
-// Your existing routes
 app.get('/', (req, res) => {
   res.send('Hello, this is my Render Server!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
